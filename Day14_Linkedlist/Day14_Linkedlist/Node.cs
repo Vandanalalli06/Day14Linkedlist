88a33485b33c38722b8651ea57d480618c916465
Vandanalalli06 <vandanalalli06@gmail.com>
2022-07-27 22:56:57 +0530

    Welcome to Linked list program

---
+++ Day14_Linkedlist/Day14_Linkedlist/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_Linkedlist
+{
+    internal class Node//2 global variables
+    {
+        public int data;//if we would not initialize the data value then the default value will be 0 as this is having datatype Int
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Day14_Linkedlist/Day14_Linkedlist/Program.cs
@@ -0,0 +1,10 @@
+﻿using Day14_Linkedlist;
+
+Console.WriteLine("Welcome to Data Structure Linked List :\n");
+LinkedList list = new LinkedList();
+list.Add(56);
+list.Add(30);
+list.Add(70);
+Console.WriteLine("enter the data which you want to search in Linked List ");
+int data = int.Parse(Console.ReadLine());
+//Console.WriteLine(list.Search(data));//
