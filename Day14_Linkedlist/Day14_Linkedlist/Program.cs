df311f0c020d558f99bc4b9ec1b9959349f7ed2d
Vandanalalli06 <vandanalalli06@gmail.com>
2022-07-27 23:15:58 +0530

    Ability to create Linked List

--- Day14_Linkedlist/Day14_Linkedlist/Program.cs
+++ Day14_Linkedlist/Day14_Linkedlist/Program.cs
@@ -5,6 +5,5 @@ LinkedList list = new LinkedList();
 list.Add(56);
 list.Add(30);
 list.Add(70);
-Console.WriteLine("enter the data which you want to search in Linked List ");
-int data = int.Parse(Console.ReadLine());
-//Console.WriteLine(list.Search(data));//
+Console.Write("\nLinked List : ");
+list.Display();
